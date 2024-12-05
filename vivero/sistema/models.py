ac83cce7e38d396dc0e710e3e72c5255a397e4e5
marglezc <a19201105@alumnos.uady.mx>
2024-12-05 00:43:26 -0600

    cambios mar

--- vivero/sistema/models.py
+++ vivero/sistema/models.py
@@ -1,12 +1,12 @@
 from django.db import models as m
 
 class Planta(m.Model):
-    nombre = m.CharField(max_length=50)
-    tipoluz = m.CharField(max_length=50)
-    tamano = m.CharField(max_length=50)
-    especie = m.CharField(max_length=50)
+    nombre = m.CharField(max_length=100)
+    tipoluz = m.CharField(max_length=20)
+    tamano = m.CharField(max_length=20)
+    especie = m.CharField(max_length=100)
     descripcion = m.TextField()
-    imagen = m.ImageField()
+    imagen = m.TextField()  
 
     def __str__(self):
         return self.nombre
--- vivero/sistema/views.py
+++ vivero/sistema/views.py
@@ -1,10 +1,16 @@
+
+import base64
+from io import BytesIO
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 from django.db import IntegrityError
+from django.core.exceptions import ValidationError
+from .models import Planta
 
 
 
@@ -21,9 +27,40 @@ def ficha(request: HttpRequest) -> HttpResponse:
     planta = request.GET.get('planta', None) 
     return render(request, "sistema/Vista_Ficha.html", {'planta': planta})
 
-def registrarPlanta(request: HttpRequest) -> HttpResponse:
-    return render(request, "sistema/Vista_GestionArchivos.html")
+def registrarPlanta(request: HttpRequest)-> HttpResponse:
+    if request.method == 'POST':
+        
+        nombre_planta = request.POST.get('nombre_planta')
+        tipo_luz = request.POST.get('tipo_luz')
+        tamano = request.POST.get('tamano')
+        especie = request.POST.get('especie')
+        descripcion = request.POST.get('descripcion')
+        imagen = request.FILES.get('imagen')  # Obtener el archivo subido
 
+        # Validar si todos los campos están completos
+        if not all([nombre_planta, tipo_luz, tamano, especie, descripcion]):
+            raise ValidationError('Todos los campos son obligatorios.')
+
+        # Convertir la imagen a base64 si existe
+        imagen_base64 = None
+        if imagen:
+            # Leer la imagen en binario
+            image_data = imagen.read()
+            # Convertirla a base64
+            imagen_base64 = base64.b64encode(image_data).decode('utf-8')
+
+        planta = Planta.objects.create(
+            nombre=nombre_planta,
+            tipoluz=tipo_luz,
+            tamano=tamano,
+            especie=especie,
+            descripcion=descripcion,
+            imagen=imagen_base64  # Guardamos la imagen como un string base64
+        )
+
+        return redirect('catálogo') 
+
+    return render(request, 'sistema/Vista_GestionArchivos.html')
 
 def indice(request: HttpRequest) -> HttpResponse:
     return render(request, "sistema/Vista_Indice.html")
