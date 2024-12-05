6e53179e82a8255fe50ec751c391ee38865c3ab2
Ricardo Córdova <a19203686@alumnos.uady.mx>
2024-12-05 01:15:00 -0600

    base actualizada

--- vivero/sistema/models.py
+++ vivero/sistema/models.py
@@ -1,4 +1,7 @@
 from django.db import models as m
+import qrcode
+import os
+from django.conf import settings
 
 class Planta(m.Model):
     nombre = m.CharField(max_length=100)
@@ -6,9 +9,30 @@ class Planta(m.Model):
     tamano = m.CharField(max_length=20)
     especie = m.CharField(max_length=100)
     descripcion = m.TextField()
-    imagen = m.TextField()  
+    imagen = m.ImageField()
+    qr_code = m.ImageField(default="qr_codes/default_qr.png")
 
     def __str__(self):
         return self.nombre
+    
+    def generate_qr_img(self):
+
+        url_qr = f"{settings.SITE_URL}/ficha?planta={self.nombre}"
+
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        qr.add_data(url_qr)
+        qr.make(fit=True)
+
+        qr_img = qr.make_image(fill='black', back_color='white')
+        qr_path = f"qr_codes/{self.nombre}_qr.png"
+        full_path = os.path.join(settings.MEDIA_ROOT, qr_path)
+        qr_img.save(full_path)
+        self.qr_code = qr_path
+        self.save()
 
 
